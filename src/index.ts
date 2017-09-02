d2ca2fe99531db4f6b3a3b4558382e1e2d3e179d
Michael Schramek <mail@michaelschramek.com>
2017-09-02 10:25:04 +0200

    add angularjs example

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,30 @@
-import * as _ from "lodash";
+import * as angular from 'angular';
 
-function component() {
-    var element = document.createElement('div');
-  
-    // Lodash, currently included via a script, is required for this line to work
-    element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-  
-    return element;
-  }
-  
-  document.body.appendChild(component());
+angular.module('migrationApp', [])
+.controller('TodoListController', function() {
+  var todoList = this;
+  todoList.todos = [
+    {text:'learn AngularJS', done:true},
+    {text:'build an AngularJS app', done:false}];
+
+  todoList.addTodo = function() {
+    todoList.todos.push({text:todoList.todoText, done:false});
+    todoList.todoText = '';
+  };
+
+  todoList.remaining = function() {
+    var count = 0;
+    angular.forEach(todoList.todos, function(todo) {
+      count += todo.done ? 0 : 1;
+    });
+    return count;
+  };
+
+  todoList.archive = function() {
+    var oldTodos = todoList.todos;
+    todoList.todos = [];
+    angular.forEach(oldTodos, function(todo) {
+      if (!todo.done) todoList.todos.push(todo);
+    });
+  };
+});
